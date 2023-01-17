54d7df059b1ee0e0bc28d80d7b14ca59ef1b448a
aldegad <aldegad@naver.com>
2023-01-17 09:02:22 +0900

    router fix

--- local_modules/router/src/useRouter.tsx
+++ local_modules/router/src/useRouter.tsx
@@ -5,32 +5,42 @@ type ParamListBase = {
   [x: string]: object | undefined;
 }
 type Keyof<T extends {}> = Extract<keyof T, string>;
+type Href = string | UrlObject & { target?:string };
+
 export type RouterProps<T extends ParamListBase, K extends keyof T = Keyof<T>> = T[K];
 
 export function useRouter<Query extends RouterProps<ParamListBase>>() {
   const { pathname, query:_query, push:_push, replace:_replace, back:_back, isReady } = useNextRouter();
   const query = _query as Query;
-  const push = (href:string | UrlObject & { target?:string }, as?: Url) => {
-    if(typeof href !== 'string' && href.target && href.target) {
-      let url = href.pathname || '/';
-      if(href.query) {
-        const entries = Object.entries(href.query);
-        url += '?' + entries.map(([key, value]) => `${key}=${value}`).join('&');
-      }
-      window.open(url, href.target);
-    }
-    else {
-      _push(href, as);
-    }
+  const push = (href:Href, as?: Url) => {
+    const newWindow = openWindow(href);
+    if(newWindow) return newWindow;
+    _push(href, as);
   }
-  const reset = (href:string | UrlObject & { target?:string }, as?: Url) => {
+  const reset = (href:Href, as?: Url) => {
+    const newWindow = openWindow(href);
+    if(newWindow) return newWindow;
     _replace(href, as);
   }
-  const replace = (href:string | UrlObject & { target?:string }, as?: Url) => {
+  const replace = (href:Href, as?: Url) => {
+    const newWindow = openWindow(href);
+    if(newWindow) return newWindow;
     _replace(href, as);
   }
   const back = () => {
     _back();
   }
   return { query, push, reset, replace, back, pathname, isReady };
+}
+
+function openWindow(href:Href) {
+  if(typeof href === 'string') return null;
+  if(!href.target) return null;
+
+  let url = href.pathname || '/';
+  if(href.query) {
+    const entries = Object.entries(href.query);
+    url += '?' + entries.map(([key, value]) => `${key}=${value}`).join('&');
+  }
+  return window.open(url, href.target);
 }
