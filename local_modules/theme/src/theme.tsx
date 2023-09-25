7ae1ed3a07bf54ce92fefa1c682ef63ececa8d6d
aldegad <aldegad@naver.com>
2023-09-25 19:36:21 +0900

    react-theme add useMemo

--- local_modules/theme/src/theme.tsx
+++ local_modules/theme/src/theme.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 const ThemeContext = createContext(null) as any;
 
@@ -35,14 +35,16 @@ export function ThemeProvider<S extends Color,T extends Function>({children, col
     }
   }, [])
 
+  const themeValue = useMemo(() => ({
+    ...theme(color[colorScheme || 'light']),
+    colorScheme,
+    darkModeEnabled
+  }), [colorScheme]);
+
   return (
     <div style={{ visibility: colorScheme ? 'visible' : 'hidden' }}>
       <ThemeContext.Provider
-        value={{
-          ...theme(color[colorScheme || 'light']),
-          colorScheme,
-          darkModeEnabled
-        }}>
+        value={themeValue}>
         {children}
       </ThemeContext.Provider>
     </div>
