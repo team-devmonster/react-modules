30ec1efec4efcc4edc3e1a87f09f005b05012362
mintaek <mtseo@devmonster.co.kr>
2023-12-28 10:54:38 +0900

    fix Row tag

--- local_modules/tags/src/row.tsx
+++ local_modules/tags/src/row.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react';
 
 export type RowPorps = {
-  children: ReactNode;
+  children?: ReactNode;
 };
 
-export default function Row({ children }: RowPorps) {
+export function Row({ children }: RowPorps) {
   return <div style={{ flexDirection: 'row', display: 'flex'}}>{children}</div>;
 }
