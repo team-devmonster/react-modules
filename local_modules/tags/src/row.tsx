f07fda98918586455bc330b86b9da1a6aed6cea8
mintaek <mtseo@devmonster.co.kr>
2023-12-28 10:59:20 +0900

    update Row Tag

--- local_modules/tags/src/row.tsx
+++ local_modules/tags/src/row.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
 export type RowPorps = {
   children?: ReactNode;
+  style?: CSSProperties;
 };
 
-export function Row({ children }: RowPorps) {
-  return <div style={{ flexDirection: 'row', display: 'flex'}}>{children}</div>;
+export function Row({ children, style }: RowPorps) {
+  return (
+    <div style={{ flexDirection: 'row', display: 'flex', ...style }}>
+      {children}
+    </div>
+  );
 }
